f6a8054fdc312a7277c3bfbccf5605ccad83a004
tasajjad <79570092+tasajjad@users.noreply.github.com>
2021-09-24 21:06:30 +0600

    Push

---
+++ asate/js/comon.js
@@ -0,0 +1,32 @@
+
+
+// $(document).ready(function(){
+
+
+// 	$('.featured-img').hover(function(){
+
+// 		$('.featured-img-icon').fadeIn();
+
+// 		// $('.featured-img-icon').show()
+
+
+
+// 	});
+
+
+// 	$('.featured-img').mouseleave(function(){
+
+// 		$('.featured-img-icon').fadeOut();
+
+			 
+
+
+
+// 	});
+
+
+
+
+
+
+});
